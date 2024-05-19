26c31358f71dafaad0dada81e8821d0cdf01cb08
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2024-05-19 15:22:44 -0400

    Modifica import desafio_guiado9.*;

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
-import desafio_evaluado6.FechasGermanFuentes;
-import desafio_evaluado6.FechasGermanFuentes;
-import desafio_guiado9.CambiarWhile;
-import desafio_guiado9.OnlyEven;
-import desafio_guiado9.SumaImpar;
-import desafio_guiado9.SumarImparRango;
-import desafio_guiado9.Fibonacci;
-import dia7.MenuOpcionesUsuario;
-import dia7.Password;
-import dia7.While;
-import dia_5.NumeroPar;
-import dia_8.*;
-
+import desafio_guiado9.*;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {

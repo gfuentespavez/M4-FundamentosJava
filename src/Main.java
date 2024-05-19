8ba2f61240e671691452945ff1127cc16d52ea52
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2024-05-19 16:41:31 -0400

    Trabajando día 10

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import dia_5.*;
+import desafio_evaluado6.*;
+import dia7.*;
+import dia_8.*;
 import desafio_guiado9.*;
+import dia_10.*;
+
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        /*Día 9
         // CambiarWhile
         CambiarWhile obj1 = new CambiarWhile();
         obj1.CambiarDoWhile();
@@ -29,9 +35,20 @@ public class Main {
         // Mostrar secuencia Fibonacci
         Fibonacci obj6 = new Fibonacci();
         obj6.Fibonacci(sc);
+         */
 
         //Cerrar Scanner
-        sc.close();
 
+        /*
+        //Tablas de multiplicar
+        Tables obj7 = new Tables();
+        obj7.showTables();
+        */
+
+        //Asteriscos
+        Stars obj8 = new Stars();
+        obj8.Stars(sc);
+
+        sc.close();
     }
 }

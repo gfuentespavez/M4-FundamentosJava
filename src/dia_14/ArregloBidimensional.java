b7d075aff98dfa45e2598482a314bb9152c6c097
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2024-06-16 21:23:14 -0400

    Arreglo bidimensional

---
+++ src/dia_14/ArregloBidimensional.java
@@ -0,0 +1,10 @@
+package dia_14;
+
+public class ArregloBidimensional {
+    public void ArregloBidimensional () {
+        int n = 3;
+        int matrizEjemplo [][] = {{1,2,3}, {4,5,6}, {7,8,9}};
+            for (int i = 0; i < matrizEjemplo.length; i++)
+                    System.out.print(matrizEjemplo[0][i] + " ");
+    }
+}

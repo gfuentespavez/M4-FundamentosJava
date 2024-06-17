294d842152fc2cfbaa8e0d6bf7c53ba4ed06dc8b
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2024-06-16 21:23:01 -0400

    Agregar Par

---
+++ src/dia_13/AgregarPar.java
@@ -0,0 +1,13 @@
+package dia_13;
+import java.util.ArrayList;
+
+public class AgregarPar {
+    public void AgregarPar () {
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        Integer numero = 0;
+        if(numero%2 == 0) {
+            numeros.add(numero);
+        }
+        System.out.println(numeros);
+    }
+}

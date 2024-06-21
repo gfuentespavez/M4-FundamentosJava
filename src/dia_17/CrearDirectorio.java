c9c6e8bb1acb594a70184bf59d4e0234e7d4312d
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2024-06-21 01:22:12 -0400

    Crea directorio en carpeta src. Ejecutable desde Main.java.

---
+++ src/dia_17/CrearDirectorio.java
@@ -0,0 +1,29 @@
+package dia_17;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CrearDirectorio {
+    public void crearDirectorio(String nombre) {
+        File directorio = new File("src" + File.separator + nombre);
+
+        if (!directorio.exists()) {
+            boolean isCreated = directorio.mkdir();
+            if (isCreated) {
+                System.out.println("Directorio " + nombre + " creado");
+            } else {
+                System.out.println("No se puede crear el directorio " + nombre);
+            }
+        } else {
+            System.out.println("El directorio " + nombre + " ya existe");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CrearDirectorio cd = new CrearDirectorio();
+        System.out.println("Introduce el nombre del directorio");
+        String dirnName = sc.nextLine();
+        cd.crearDirectorio(dirnName);
+    }
+}

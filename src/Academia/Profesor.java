f4397666c17070a5f0f75c3f3a907155147a374a
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2024-07-27 21:59:11 -0400

    Clase Profesor para ejemplo de Polimorfismo.

---
+++ src/Academia/Profesor.java
@@ -0,0 +1,8 @@
+package Academia;
+
+//Extensión en la clase Profesor
+public class Profesor extends Persona {
+    public Profesor(String rut, String nombre, boolean presente) {
+        super(rut, nombre, presente);
+    }
+}

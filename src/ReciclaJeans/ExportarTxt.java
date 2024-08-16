994ee1585043b332415fbb616e64333a8f4ce43f
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2024-08-16 17:05:47 -0400

    Clase para exportar la lista de productos.

--- src/ReciclaJeans/ExportarTxt.java
+++ src/ReciclaJeans/ExportarTxt.java
@@ -1,24 +1,23 @@
-package IndianaJeans;
+package ReciclaJeans;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class ExportarTxt extends Exportador{
+public class ExportarTxt extends Exportador {
     private String nombreArchivo;
 
-    //Constructor para inicializar el nombre del archivo
     public ExportarTxt(String nombreArchivo) {
         super();
         this.nombreArchivo = nombreArchivo;
     }
 
-    //Implementacióm del método exportar
     @Override
-    public void exportar(ArrayList<String> listaProductos) {
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
-            for (String producto : listaProductos) {
-                writer.write(producto);
+    public void exportar(ArrayList<Producto> listaProductos) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
+            for (Producto producto : listaProductos) {
+                writer.write(producto.toString());
                 writer.newLine();
             }
             System.out.println("Productos exportados a: " + nombreArchivo);
